e43efd3801b01159fbe9c77c38fbc584f4f8b0bc
kaoru-k <kaoru_kimura@outlook.com>
2018-08-28 15:24:40 +0900

    GoogleMaps Test

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {
+  GoogleMaps, GoogleMap, GoogleMapsEvent
+} from '@ionic-native/google-maps';
+
 
 @Component({
   selector: 'page-map',
-  templateUrl: 'map.html'
+  templateUrl: 'map.html',
 })
 export class MapPage {
+  
+  map: GoogleMap;
 
   constructor(public navCtrl: NavController) {
+  
+  }
 
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+
+    this.map = GoogleMaps.create('map');
+
+    this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+      console.log('GoogleMaps Loaded.');
+    });
   }
 
 }
