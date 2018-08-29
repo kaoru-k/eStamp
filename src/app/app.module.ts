d2d9e6110bfd3275af5b264450627c0a94b1833a
kaoru-k <kaoru_kimura@outlook.com>
2018-08-29 13:50:15 +0900

    Add GeoLocation Plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StampBookPage } from '../pages/stamp-book/stamp-book';
 import { SettingsPage } from '../pages/settings/settings';
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { Geolocation } from '@ionic-native/geolocation';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -34,6 +35,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 declare var google;
 
@@ -12,15 +13,33 @@ export class MapPage {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public geolocation: Geolocation, public alertCtrl: AlertController) {
   
   }
 
   ionViewDidLoad(){
-    this.loadMapJS();
+    this.showLocation();
+    // this.loadMapJS();
+  }
+
+  showLocation() {
+    this.geolocation.getCurrentPosition().then((position) => {
+      let latlng = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      }
+      let alert = this.alertCtrl.create({
+        title: 'Lat and Lng',
+        subTitle: 'lat = ' + latlng.lat + ' , lng = ' + latlng.lng,
+        buttons: ['Close']
+      });
+      alert.present();
+
+    });
   }
 
   loadMapJS() {
+
     this.map = new google.maps.Map(this.mapElement.nativeElement, {
       center: {lat: 34.075, lng: 134.515},
       zoom: 8
