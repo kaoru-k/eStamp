afcdb39debdba6308ca7277ded58d41643abf704
kaoru-k <kaoru_kimura@outlook.com>
2018-09-10 15:37:04 +0900

    Open StampDialog

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,7 +4,6 @@ import { NavController } from 'ionic-angular';
 import { MapPage } from '../map/map';
 import { StampBookPage } from '../stamp-book/stamp-book';
 import { SettingsPage } from '../settings/settings';
-import { GetStampPage } from '../get-stamp/get-stamp';
 
 @Component({
   templateUrl: 'tabs.html'
