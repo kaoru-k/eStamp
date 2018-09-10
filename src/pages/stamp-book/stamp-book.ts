8672ecf6052ee33a74995ef28ebab1e941c4ccaa
kaoru-k <kaoru_kimura@outlook.com>
2018-09-10 15:36:12 +0900

    Open StampDialog

--- src/pages/stamp-book/stamp-book.ts
+++ src/pages/stamp-book/stamp-book.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ModalController } from 'ionic-angular/components/modal/modal-controller';
 import { GetStampPage } from '../get-stamp/get-stamp';
+import { StampDialogPage } from '../stamp-dialog/stamp-dialog';
 
 @Component({
   selector: 'page-stamp-book',
@@ -10,6 +11,11 @@ import { GetStampPage } from '../get-stamp/get-stamp';
 export class StampBookPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController) {
+  }
+
+  stampOnClick() {
+    let myModal = this.modalCtrl.create(StampDialogPage);
+    myModal.present();
   }
 
   getStampButtonOnClick() {
