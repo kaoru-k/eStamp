41414bac5a20973750df7e9f4738df40ca24957d
kaoru-k <kaoru_kimura@outlook.com>
2018-09-13 11:03:54 +0900

    Use ngFor

--- src/pages/stamp-book/stamp-book.ts
+++ src/pages/stamp-book/stamp-book.ts
@@ -3,23 +3,44 @@ import { NavController, NavParams } from 'ionic-angular';
 import { ModalController } from 'ionic-angular/components/modal/modal-controller';
 import { GetStampPage } from '../get-stamp/get-stamp';
 import { StampDialogPage } from '../stamp-dialog/stamp-dialog';
+import { Storage } from '@ionic/storage'
 
 @Component({
   selector: 'page-stamp-book',
   templateUrl: 'stamp-book.html',
 })
 export class StampBookPage {
+  public stampList: any[] = [];
+  public stampCount: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController, public storage: Storage) {
   }
 
-  stampOnClick() {
-    let myModal = this.modalCtrl.create(StampDialogPage);
-    myModal.present();
+  ionViewDidLoad() {
+    this.storage.get('spotList').then((csvData) => {
+      this.stampCount = 0;
+      csvData.forEach((row) =>{
+        if (row['Get'] == true) {this.stampCount++}
+        this.stampList.push({
+          ID: "No." + ("000" + row['ID']).slice(-3),
+          Name: row['Name'],
+          Get: true,
+          GetDate: row['GetDate'],
+          img: "assets/imgs/stamp/stamp_sample.png"
+        });
+      });
+    });
+  }
+
+  stampOnClick(id, name, get, date) {
+    if (get == true) {
+      let myModal = this.modalCtrl.create(StampDialogPage, {id: id, name: name, date: date});
+      myModal.present();
+    }
   }
 
   getStampButtonOnClick() {
-    let myModal = this.modalCtrl.create(GetStampPage);
+    let myModal = this.modalCtrl.create(GetStampPage, {});
     myModal.present();
   }
   
