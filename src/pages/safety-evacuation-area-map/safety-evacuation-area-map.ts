327a23e2019d37e337e982be640a17cbd8c939f3
kaoru-k <kaoru_kimura@outlook.com>
2018-09-29 16:56:23 +0900

    修正

--- src/pages/safety-evacuation-area-map/safety-evacuation-area-map.ts
+++ src/pages/safety-evacuation-area-map/safety-evacuation-area-map.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { GoogleMaps, GoogleMap, GoogleMapOptions } from '@ionic-native/google-maps';
 
 /**
  * Generated class for the SafetyEvacuationAreaMapPage page.
@@ -13,12 +14,37 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'safety-evacuation-area-map.html',
 })
 export class SafetyEvacuationAreaMapPage {
+  googleMap: GoogleMap;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SafetyEvacuationAreaMapPage');
+    this.loadMap();
+  }
+
+  loadMap() {
+    let options: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 34.077,
+          lng: 134.560
+        },
+        zoom: 12
+      },
+      controls: {
+        'myLocationButton': true,
+        'myLocation': true,
+      }
+    }
+    this.googleMap = GoogleMaps.create('map_canvas',options);
+    // this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+    //   this.putMarkers();
+    // });
+  }
+  
+  putMarkers() {
+  
   }
 
 }
