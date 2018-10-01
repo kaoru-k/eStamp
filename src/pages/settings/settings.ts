64b6ac8e17bc48e58841b9df844b7390d745f35a
kaoru-k <kaoru_kimura@outlook.com>
2018-10-01 21:25:21 +0900

    Language Select

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -11,11 +11,57 @@ import { CopyrightNoticePage } from '../copyright-notice/copyright-notice'
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public storage: Storage, private alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public storage: Storage, private alertCtrl: AlertController ) {
   }
 
   copyrightNoticeButtonOnClick() {
     this.navCtrl.push(CopyrightNoticePage);
+  }
+
+  languageSelect() {
+    let langSelect = this.alertCtrl.create();
+    langSelect.setTitle('Language Select');
+
+    langSelect.addInput({
+      type: 'radio',
+      label: '日本語',
+      value: 'ja_JP',
+      checked: true
+    });
+
+    langSelect.addInput({
+      type: 'radio',
+      label: 'English',
+      value: 'en_US'
+    });
+
+    langSelect.addInput({
+      type: 'radio',
+      label: '中文(简体)',
+      value: 'zh_CN'
+    });
+
+    langSelect.addInput({
+      type: 'radio',
+      label: '中文(繁體)',
+      value: 'zh_TW'
+    });
+
+    langSelect.addInput({
+      type: 'radio',
+      label: '한국어',
+      value: 'ko_KR'
+    });
+
+    langSelect.addButton('Cancel');
+    langSelect.addButton({
+      text: 'OK',
+      handler: (data: any) => {
+        this.storage.set('language', data);
+      }
+    });
+
+    langSelect.present();
   }
 
   deleteSettings() {
